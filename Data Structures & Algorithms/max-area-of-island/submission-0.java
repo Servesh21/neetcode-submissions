eda35e6342ea84986e00afc57239c76974115fb5
Servesh Khade <161584037+Servesh21@users.noreply.github.com>
2026-08-07 22:49:38 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,46 @@
+class Pair{
+    int row,col;
+    Pair(int r,int c){
+        this.row = r;
+        this.col = c;
+    }
+}
+class Solution {
+    public int bfs(int r,int c, int[][] vis,int[][] grid){
+         int n = grid.length,m = grid[0].length;
+         Queue<Pair> q = new LinkedList<>();
+         q.offer(new Pair(r,c));
+         vis[r][c] =1;
+         int[] dRow = new int[]{-1,0,1,0};
+         int[] dCol = new int[]{0,1,0,-1};
+         int count=1;
+         while(!q.isEmpty()){
+            int row = q.peek().row,col= q.peek().col;
+            q.poll();
+         for(int i=0;i<4;i++){
+            int nr = row+dRow[i],nc = col + dCol[i];
+            if(nr >=0 && nc >=0 && nr < n && nc < m && vis[nr][nc] == 0 && grid[nr][nc] == 1){
+                q.offer(new Pair(nr,nc));
+                vis[nr][nc] = 1;
+                count++;
+            }
+         }
+         
+         }
+         return count;
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        int n = grid.length,m = grid[0].length;
+        int[][] vis= new int[n][m];
+        int area =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1 && vis[i][j] ==0){
+                    
+                    area = Math.max(area,bfs(i,j,vis,grid));
+                }
+            }
+        }
+        return area;
+    }
+}
